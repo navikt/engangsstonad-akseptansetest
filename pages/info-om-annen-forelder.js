52d826e3d919eb2935ae6ebaa40bbd60ce96be99
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-08-24 15:19:10 +0200

    added a new test

---
+++ pages/info-om-annen-forelder.js
@@ -0,0 +1,12 @@
+import { Selector } from 'testcafe'
+
+export class AnnenForelderPage {
+
+   constructor() {
+      this.navn = Selector('#js-annenForelder')
+      this.fnr = Selector('#js-fødselsnummer')
+      this.neste = Selector('button.knapp:nth-child(3)')
+   }
+
+}
+
---
+++ tests/kvinneover18_2_barn_fødsel_ikke_eøs_kun_i_norge.test.js
@@ -0,0 +1,56 @@
+import { config } from '../config'
+import { LoginPage } from '../pages/login'
+import { WelcomePage } from '../pages/welcome'
+import { InfoOmBarnPage } from '../pages/info-om-barn'
+import { TilknytningTilNorgePage } from '../pages/tilknytning-til-norge'
+import { OppsummeringPage } from '../pages/oppsummering'
+import { KvitteringPage } from '../pages/kvittering'
+import { AnnenForelderPage } from '../pages/info-om-annen-forelder'
+
+const loginPage = new LoginPage()
+const welcomePage = new WelcomePage()
+const infoOmBarnetPage = new InfoOmBarnPage
+const tilknytningTilNorgePage = new TilknytningTilNorgePage()
+const oppsummeringPage = new OppsummeringPage()
+const annenForelderPage = new AnnenForelderPage()
+const kvitteringPage = new KvitteringPage()
+
+const dateFormat = 'DD.MM.YYYY'
+const moment = require('moment')
+
+const today = moment().format(dateFormat)
+const tomorrow = moment().add(1, 'days').format(dateFormat)
+
+fixture('Engangsstønad application')
+   .beforeEach(async t => {
+      await t.useRole(loginPage.login(config.fnr_ikkenordisk_eøs))
+   })
+
+test('must be sent without errors', async t => {
+   await t
+      .navigateTo(config.url)
+      .click(welcomePage.forståttChecker)
+      .click(welcomePage.startButton)
+      .click(infoOmBarnetPage.fødselFramtid)
+      .click(infoOmBarnetPage.tvillinger)
+      .typeText(infoOmBarnetPage.termindato, tomorrow)
+      .setFilesToUpload(infoOmBarnetPage.fileUpload, ['../upload.jpg'])
+      .typeText(infoOmBarnetPage.terminbekreftelse, today)
+      .pressKey('tab')
+      .click(infoOmBarnetPage.neste)
+
+      .typeText(annenForelderPage.navn, 'Jan Johansen')
+      .typeText(annenForelderPage.fnr, config.fnr_mann_norsk)
+      .pressKey('tab')
+      .click(annenForelderPage.neste)
+
+      .click(tilknytningTilNorgePage.siste12INorge)
+      .click(tilknytningTilNorgePage.neste12INorge)
+      .click(tilknytningTilNorgePage.iUtlandetVedFødsel)
+      .click(tilknytningTilNorgePage.neste)
+
+      .click(oppsummeringPage.bekreftRiktigeOpplysninger)
+      .click(oppsummeringPage.sendInn)
+      .expect(kvitteringPage.melding.innerText).contains('Takk for søknaden')
+})
+
